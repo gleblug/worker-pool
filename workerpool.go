e85a2dc0b8315eafbb6a7de9e070f01f63cc2a8a
Lugovtsov Gleb <galera951@gmail.com>
2024-10-30 13:24:14 +0300

    Remove func implementation

--- workerpool.go
+++ workerpool.go
@@ -37,24 +37,36 @@ func (wp *WorkerPool) Run() {
 
 func (wp *WorkerPool) Add(count int) {
 	newWorkers := make([]Worker, count)
+	wp.mu.Lock()
+	currentCount := len(wp.workers)
+	wp.mu.Unlock()
+
 	for i := range newWorkers {
-		w := NewWorker(i + len(wp.workers))
+		w := NewWorker(i + currentCount)
 
 		wp.wg.Add(1)
 		go w.Work(&wp.wg, wp.input, wp.output)
 
 		newWorkers[i] = w
 	}
+
 	wp.mu.Lock()
 	wp.workers = append(wp.workers, newWorkers...)
 	wp.mu.Unlock()
 }
 
-// func (wp *WorkerPool) Remove(count int) {
-// 	// TODO: add count < workers count check
-// 	for i := wp.workersCount - count; i < wp.workersCount; i++ {
-// 		// TODO: wait until workers is really deactivate (wait group)
-// 		go wp.workers[i].Deactivate()
-// 	}
-// 	wp.workers = wp.workers[:wp.workersCount-count]
-// }
+func (wp *WorkerPool) Remove(count int) {
+	// TODO: add count < workers count check
+	wp.mu.Lock()
+	currentCount := len(wp.workers)
+	wp.mu.Unlock()
+	
+	for i := currentCount - count; i < currentCount; i++ {
+		// TODO: wait until workers is really deactivate (wait group)
+		go wp.workers[i].Deactivate()
+	}
+
+	wp.mu.Lock()
+	wp.workers = wp.workers[:currentCount-count]
+	wp.mu.Unlock()
+}
